9d2d827c16af65e18df5be406bbc133e34a500b2
Uswatul Hasanah <230705033@student.ar-raniry.ac.id>
2025-05-19 10:37:32 +0700

    Created using Colab

---
+++ object.py
@@ -0,0 +1,52 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOWhcHlB4w4zdRrN1kq/NsH",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/uswatl/praktikum_PBO_UswatulHasanah-/blob/main/object.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "lo_fpd-5naaH"
+      },
+      "outputs": [],
+      "source": [
+        "class Dog:\n",
+        "    species = \"Canine\"  # Class attribute\n",
+        "\n",
+        "    def __init__(self, name, age):\n",
+        "        self.name = name  # Instance attribute\n",
+        "        self.age = age  # Instance attribute\n",
+        "\n",
+        "# Creating an object of the Dog class\n",
+        "dog1 = Dog(\"Buddy\", 3)\n",
+        "\n",
+        "print(dog1.name)\n",
+        "print(dog1.species)"
+      ]
+    }
+  ]
+}
